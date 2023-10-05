efe2c15616b59326ca696072dd0714509883b082
jayprajapati009 <jayprajapati9900@gmail.com>
2023-10-05 15:05:48 -0400

    All test passing

--- test/test.cpp
+++ test/test.cpp
@@ -7,4 +7,4 @@ TEST(dummy_test, this_should_pass) { EXPECT_EQ(1, 1); }
 
 TEST(dummy_test, this_should_pass_too) { EXPECT_EQ(my_function1(3), 3); }
 
-TEST(dummy_test, this_will_fail) { EXPECT_EQ(my_function2(3.2), 3); }
+TEST(dummy_test, this_will_fail) { EXPECT_EQ(my_function2(3), 3); }
